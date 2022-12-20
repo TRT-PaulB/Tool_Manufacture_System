344f859f1564ef12b7c56c1fb64a7bedbec193dd
TRT-PaulB <paul.bickell@raytheon.co.uk>
2022-12-20 16:26:55 +0000

    initial structure first steps

---
+++ central/src/main/java/uk/co/toolmaker/central/ToolManufactureEngineApplication.java
@@ -0,0 +1,13 @@
+package uk.co.toolmaker.central;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolManufactureEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToolManufactureEngineApplication.class, args);
+	}
+
+}
---
+++ central/src/test/java/uk/co/toolmaker/central/ToolManufactureEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.co.toolmaker.central;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolManufactureEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
